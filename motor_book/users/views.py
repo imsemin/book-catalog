99b6d2c902834f6da10109222d07e7fa20d732f0
Evgeniy Semin <evgeniy.semin91@gmail.com>
2022-07-13 23:01:05 +0300

    added user profile views

--- motor_book/users/views.py
+++ motor_book/users/views.py
@@ -1,10 +1,18 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.contrib.auth import get_user_model
+from django.views.generic import CreateView, DetailView
 
 from .forms import CreationForm
+
+User = get_user_model()
 
 
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("books:homepage")
     template_name = "users/signup.html"
+
+
+class ProfileDetailView(DetailView):
+    model = User
+    template_name = "users/profile.html"
